a32b3dd84531b04f471b88551027bbc13714d3e4
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2020-07-21 11:36:18 +0200

    Create deck class

---
+++ wiezen-library/Deck.py
@@ -0,0 +1,26 @@
+from Card import Card
+import random
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.reset()
+
+    def shuffle(self, times=1):
+        for i in range(0, times):
+            self.cards = random.shuffle(self.cards)
+
+    def get_card(self):
+        return self.cards.pop()
+
+    def reset(self):
+        self.cards = [Card(t, v) for t in range(0, 3) for v in range(1,14)]
+
+    def __repr__(self):
+        return str(self.cards)
+
+
+if __name__ == '__main__':
+    d = Deck()
+    print(d)
