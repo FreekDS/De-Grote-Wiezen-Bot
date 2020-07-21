cdf852ac3601d3c55e710993f539315e007a183e
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2020-07-21 10:52:55 +0200

    de wiezenbot is een feit

---
+++ DiscordBot.py
@@ -0,0 +1,20 @@
+# bot.py
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
