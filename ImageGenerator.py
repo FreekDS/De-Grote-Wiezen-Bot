8415a603c5986a6c0aebf2bcd74617f0a7b921cf
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2020-07-26 13:58:27 +0200

    Generate tables where cards are not played yet

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageFont, ImageDraw
-from typing import Dict
+from typing import List, Tuple
 from wiezenlibrary.Card import Card, CardType, Beilekes
 from wiezenlibrary.Player import Player
 import os
@@ -34,8 +34,10 @@ class ImageGenerator:
         for image in images:
             new_image.paste(image, (x_offset, 0))
             x_offset += image.size[0] + offset
+            image.close()
 
         new_image.save(f'{OUTPUT}/hand.png')
+        new_image.close()
 
     @staticmethod
     def remove_hand():
@@ -48,14 +50,18 @@ class ImageGenerator:
     def _get_card_img(self, card: Card):
         return f"{INPUT}/decks/{self.deck}/{card.image_name}.png"
 
-    def generate_table(self, table_layout: Dict[Player, Card], offset=20):
+    def _get_card_size(self):
+        with Image.open(self._get_card_img(Card(1, 1))) as image:
+            return image.size
+
+    def generate_table(self, table_layout: List[Tuple[Player or None, Card or None]], offset=20):
         images = list()
         names = list()
-        for player, card in table_layout.items():
-            images.append(Image.open(self._get_card_img(card)))
-            names.append(player.name)
+        for player, card in table_layout:
+            images.append(Image.open(self._get_card_img(card)) if card else None)
+            names.append(player.name if player else None)
 
-        single_width, single_height = images[0].size
+        single_width, single_height = self._get_card_size()
 
         width = 3 * single_width + 4 * offset
         height = 3 * single_height + 8 * offset
@@ -70,7 +76,9 @@ class ImageGenerator:
         ]
 
         for index, image in enumerate(images):
-            new_image.paste(image, img_positions[index])
+            if image:
+                new_image.paste(image, img_positions[index])
+                image.close()
 
         draw = ImageDraw.Draw(new_image)
         font = ImageFont.truetype(f"{INPUT}/fonts/font.ttf", 60)
@@ -83,18 +91,20 @@ class ImageGenerator:
         ]
 
         for index, name in enumerate(names):
-            fill = (255, 215, 0) if list(table_layout.keys())[index].is_dealer else (255, 255, 255)
-            draw.text(text_positions[index], name, font=font, fill=fill)
+            if name:
+                fill = (255, 215, 0) if table_layout[index][0].is_dealer else (255, 255, 255)
+                draw.text(text_positions[index], name, font=font, fill=fill)
 
         new_image.save(f"{OUTPUT}/table.png")
+        new_image.close()
 
 
 if __name__ == '__main__':
-    layout = {
-        Player("jefke", "0", False): Card(CardType.KLAVEREN, Beilekes.KONING),
-        Player("louike", "1", False): Card(CardType.KOEKEN, 6),
-        Player("franske", "2", False): Card(CardType.HARTEN, Beilekes.BOER),
-        Player("mark", "3", True): Card(CardType.KOEKEN, Beilekes.DAME)
-    }
+    layout = [
+        (Player("jefke", "0", False), Card(CardType.KLAVEREN, Beilekes.KONING)),
+        (Player("louike", "1", False), Card(CardType.KOEKEN, 6)),
+        (Player("franske", "2", False), Card(CardType.HARTEN, Beilekes.BOER)),
+        (Player("mark", "3", True), None)
+    ]
 
     ImageGenerator(1).generate_table(layout)
